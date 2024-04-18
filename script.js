c70e3e49794cb3aa82f149821cdd21533246e875
Huyền Trần <tranthimyhuyen10@gmail.com>
2024-04-19 02:11:33 +0700

    update path-pdf and content

---
+++ script.js
@@ -0,0 +1,28 @@
+function downloadFile() {
+  var url = "/TRAN-THI-MY-HUYEN.pdf";
+  var filename = "BusinessAnalyst-TranThiMyHuyen.pdf";
+  var element = document.createElement("a");
+  element.setAttribute("href", url);
+  element.setAttribute("download", filename);
+  element.style.display = "none";
+  document.body.appendChild(element);
+  element.click();
+  document.body.removeChild(element);
+}
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("backTop").style.display = "block";
+  } else {
+    document.getElementById("backTop").style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
